a3a0981f7b1414b540b19ce6c9c8c0221383e4d6
Muhammad Fitrah Abdul Rahman Saleh <mfitrah15ofcc@gmail.com>
2026-03-01 18:52:09 +0700

    ujicoba

--- projectsem4/web/indahsarisalon-web/app/Http/Controllers/ProdukController.php
+++ projectsem4/web/indahsarisalon-web/app/Http/Controllers/ProdukController.php
@@ -3,31 +3,33 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Produk;
 
 class ProdukController extends Controller
 {
     public function index()
     {
-        $produks = collect([
-            (object)[
-                'id' => 1,
-                'nama' => 'Shampoo Herbal',
-                'harga' => 25000,
-                'stok' => 10
-            ],
-            (object)[
-                'id' => 2,
-                'nama' => 'Conditioner Smooth',
-                'harga' => 30000,
-                'stok' => 5
-            ],
-            (object)[
-                'id' => 3,
-                'nama' => 'Hair Mask',
-                'harga' => 40000,
-                'stok' => 8
-            ],
-        ]);
+        // $produks = collect([
+        //     (object)[
+        //         'id' => 1,
+        //         'nama' => 'CreamBath Refresh',
+        //         'harga' => 15000,
+        //         'stok' => 10
+        //     ],
+        //     (object)[
+        //         'id' => 2,
+        //         'nama' => 'Conditioner Premium',
+        //         'harga' => 25000,
+        //         'stok' => 5
+        //     ],
+        //     (object)[
+        //         'id' => 3,
+        //         'nama' => 'Hair Mask Premium',
+        //         'harga' => 48000,
+        //         'stok' => 8
+        //     ],
+        // ]);
+        $produks = Produk::all(); // ambil semua data dari tabel 'produks'
 
         return view('produk.index', compact('produks'));
     }
